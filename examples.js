d7c40bccc206516b3979a9aee1ddfa8f2b057f34
Ryan M <rm.main@gmail.com>
2015-09-18 14:46:24 -0500

    Updated my json to use handlebars

--- examples.js
+++ examples.js
@@ -4,14 +4,31 @@ $(document).ready(function() {
     $.ajax('data.json', {
         success: function(response){
             console.log(response);
+            $(function () {
+              // Grab the template script
+              var theTemplateScript = $("#entry-template").html();
 
-            for(var it = 0; it < response.length; it++){
-              $(".content-placeholder").append("<p>" + response[it].color + " " + response[it].value + "</p>");
-            }
-        },
+              // Compile the template
+              var theTemplate = Handlebars.compile(theTemplateScript);
+
+              // Pass our data to the template
+              var theCompiledHtml = theTemplate(response);
+
+              // Add the compiled html to the page
+              $('body').html(theCompiledHtml);
+            });//end function()
+
+            //This was for appending to the DOM
+            // for(var it = 0; it < response.length; it++){
+            //   $(".content-placeholder").append("<p>" + response[it].color + " " + response[it].value + "</p>");
+            // }
+        },//end success
         error: function(request, errorType, errorMessage){
             alert(errorType + " " + errorMessage);
         }
-    });
+    });//end ajax
 
-})
+
+
+
+})//end ready
